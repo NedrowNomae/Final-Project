c7842182196d2a761a90b243aae7188479fd04fd
NedrowNomae <42844326+NedrowNomae@users.noreply.github.com>
2018-10-30 15:21:02 -0400

    Updated from Brython Server: 10/30/2018 3:21:00 PM

--- blackjack.py
+++ blackjack.py
@@ -2,7 +2,7 @@
 from ggame import App, Color, LineStyle, Sprite, RectangleAsset, CircleAsset, EllipseAsset, PolygonAsset, ImageAsset
 import random
 
-cards = ['2H', '3H', '4H', '5H', '6H', '7H', '8H', '9H', '10H', 'JH', 'QH', 'KH', 'AH', '2D', '3D', '4D', '5D', '6D', '7D', '8D', '9D', '10D', 'JD', 'QD', 'KD', 'AD', '2S', '3S', '4S', '5S', '6S', '7S', '8S', '9S', '10S', 'JS', 'QS', 'KS', 'AS', '2C', '3C', '4C', '5C', '6C', '7C', '8C', '9C', '10C', 'JC', 'QC', 'KC', 'AC']
+cards = ['H2', 'H3', 'H4', 'H5', 'H6', 'H7', 'H8', 'H9', 'H10', 'JH', 'QH', 'KH', 'AH', 'D2', 'D3', 'D4', 'D5', 'D6', 'D7', 'D8', 'D9', 'D10', 'JD', 'QD', 'KD', 'AD', 'S2', 'S3', 'S4', 'S5', 'S6', 'S7', 'S8', 'S9', 'S10', 'JS', 'QS', 'KS', 'AS', 'C2', 'C3', 'C4', 'C5', 'C6', 'C7', 'C8', 'C9', 'C10', 'JC', 'QC', 'KC', 'AC']
 random.shuffle(cards)
 #print(len(cards))
 print(cards)
@@ -227,6 +227,7 @@ class AC(Sprite):
         super().__init__(AC.AC, position)
 
 s = cards[1]
+print(s)
 s((100,100))
 
 
