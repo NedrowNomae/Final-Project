2a62049343c48b78814868c9d4125f1b3003b669
NedrowNomae <42844326+NedrowNomae@users.noreply.github.com>
2018-10-30 12:33:34 -0400

    Updated from Brython Server: 10/30/2018 12:33:33 PM

--- blackjack.py
+++ blackjack.py
@@ -1,5 +1,3 @@
-print("Hello World")
-
 from ggame import App, Color, LineStyle, Sprite, RectangleAsset, CircleAsset, EllipseAsset, PolygonAsset, ImageAsset
 
 Sprite(ImageAsset("4D.png"), (100,200))
