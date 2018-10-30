e0bc88fc34cc209cd6111d8a71caeabaa8b3fafe
NedrowNomae <42844326+NedrowNomae@users.noreply.github.com>
2018-10-30 12:34:38 -0400

    Updated from Brython Server: 10/30/2018 12:34:37 PM

--- blackjack.py
+++ blackjack.py
@@ -1,8 +1,9 @@
 
 from ggame import App, Color, LineStyle, Sprite, RectangleAsset, CircleAsset, EllipseAsset, PolygonAsset, ImageAsset
 
-Sprite(ImageAsset("4D.png"), (100,200))
 
+s = Sprite(ImageAsset("4D.png"), (100,200))
+s.scale = .1
 
 
 app = App(500,500)  
