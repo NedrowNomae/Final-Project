df6da7351da20540e163e3fb33f191eff3decb32
NedrowNomae <42844326+NedrowNomae@users.noreply.github.com>
2018-10-31 12:20:33 -0400

    Updated from Brython Server: 10/31/2018 12:20:31 PM

--- blackjack.py
+++ blackjack.py
@@ -230,9 +230,6 @@ class AC(Sprite):
 cards = [H2, H3, H4, H5, H6, H7, H8, H9, H10, JH, QH, KH, AH, D2, D3, D4, D5, D6, D7, D8, D9, D10, JD, QD, KD, AD, S2, S3, S4, S5, S6, S7, S8, S9, S10, JS, QS, KS, AS, C2, C3, C4, C5, C6, C7, C8, C9, C10, JC, QC, KC, AC]
 random.shuffle(cards)
 s = cards[0]
-
-
-
 s((100,100))
 #s = s.scale(.1)
 
@@ -240,5 +237,5 @@ s((100,100))
     
 
 
-app = App(5000,2500)  
+app = App(500,500)  
 app.run()
