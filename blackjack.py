574d6bc05dcd61b5b223535e508f502a81a98aa3
NedrowNomae <42844326+NedrowNomae@users.noreply.github.com>
2018-11-06 13:13:01 -0500

    Updated from Brython Server: 11/6/2018 1:13:00 PM

--- blackjack.py
+++ blackjack.py
@@ -739,9 +739,9 @@ def stay(event):
 
     if ace == 1 and yourscore + 10 < 22:
         yourscore = yourscore + 10
-        print("yourscore: " + yourscore)
+        print("yourscore: " + str(yourscore))
     else:
-        print("yourscore: " + yourscore)
+        print("yourscore: " + str(yourscore))
     while dealerscore < 17:
         o = cards[0]
         p = o((100*n+50,25))
