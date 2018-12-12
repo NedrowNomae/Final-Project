e0b8a4c7e17931c34d163be00eea30d01a0fafbd
NedrowNomae <42844326+NedrowNomae@users.noreply.github.com>
2018-12-12 12:15:01 -0500

    Updated from Brython Server: 12/12/2018 12:15:01 PM

--- blackjack.py
+++ blackjack.py
@@ -320,7 +320,7 @@ allowbet = 1
 def rules(event):
     print("Your hand is on the bottom. The game has mostly standard rules of blackjack. You may not split. You may not surrender.")
 def pauseplay(event):
-    global q, dealerscore, yourscore, r, a, b, cards, money, o, p, yb, db, bet, ace, t, showing, n, m, dealerace, moneyshowing, placenumber, stringmoney, moneyshowing2, allowbet
+    global q, dealerscore, yourscore, r, a, b, cards, money, o, p, yb, db, bet, ace, t, showing, n, m, dealerace, moneyshowing, placenumber, stringmoney, moneyshowing2, allowbet, begin
     if int(money) <= 0:
         print("The casino throws you out because you're broke")
         t = 52
