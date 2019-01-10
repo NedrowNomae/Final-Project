9244cda309c2d1cfb635bf47e5484ea589e3e007
NedrowNomae <42844326+NedrowNomae@users.noreply.github.com>
2019-01-10 12:00:19 -0500

    Updated from Brython Server: 1/10/2019 12:00:18 PM

--- blackjack.py
+++ blackjack.py
@@ -1337,7 +1337,8 @@ def hit(event):
         if yourscore > 21.5:
             print("You busted")
             yb = 5
-            #t = 3
+            t = 3
+            allowbet = 1
             money = int(money) - int(bet)
             print("You have $" + str(money))
             print("Press space to start another round")
